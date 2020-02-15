fa4f51d533d4219b4a1e7631bfe86e80a2191cd6
DanielaGC <danielagc676@gmail.com>
2020-02-15 12:27:41 -0300

    Oof

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -5,9 +5,9 @@ module.exports = class VoiceStateUpdateReceive {
 
     async run(oldMember, newMember) {
 
-        let voiceChannel = newMember.guild.voiceStates.get(newMember.id).channelID
+        let voiceChannel = newMember.guild.voiceStates.cache.get(newMember.id).channelID
         if (voiceChannel !== null) {
-            let vc = newMember.guild.channels.get(voiceChannel)
+            let vc = newMember.guild.channels.cache.get(voiceChannel)
             if (vc.id !== "481843365055430659") return
             if (vc.members.size >= 1) {
                 vc.join().then(v => {
@@ -16,9 +16,9 @@ module.exports = class VoiceStateUpdateReceive {
                 })
             }
         }
-        let oldVoiceChannel = oldMember.guild.voiceStates.get(this.client.user.id).channelID
+        let oldVoiceChannel = oldMember.guild.voiceStates.cache.get(this.client.user.id).channelID
         if (oldVoiceChannel !== null) {
-            let oldVc = oldMember.guild.channels.get(oldVoiceChannel)
+            let oldVc = oldMember.guild.channels.cache.get(oldVoiceChannel)
             if (oldVc.members.get(this.client.user.id) && oldVc.members.size === 1) {
                 oldVc.leave()
                 this.client.emit("ready")
