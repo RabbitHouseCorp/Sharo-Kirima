a6573607e50089b4c565455ea583784cb58fc719
DanielaGC <danielagc676@gmail.com>
2020-02-15 12:32:34 -0300

    .

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -4,29 +4,29 @@ module.exports = class MessageReactionAddReceive {
         this.client = client
     }
 
-    run (reaction, user) {
+    run(reaction, user) {
         if (user.id === "297153970613387264") {
             let link = `https://canary.discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`
             let star = ["🌠"]
             let emoji = ["pink_heart", "lori_gift", "green_gift", "blue_heart", "pink_gift", "purple_heart", "green_heart", "christmas_hat", "christmas_tree", "🎄"]
-            let channel = this.client.channels.get("659196669895901185")
+            let channel = this.client.channels.cache.get("659196669895901185")
             if (star.includes(reaction.emoji.name)) {
                 let starEmbed = new MessageEmbed()
-                .setColor(this.client.colors.default)
-                .addField(`${this.client.emotes.sharo_excited} Surguiu uma estrela cadente!`, `Uma estrela cadente acabou de aparecer! Vá e pegue ela rápido antes que o tempo esgote!`)
-                .addField(`${this.client.emotes.sharo_dying} Pegue rápido`, `🌠 Você pode pegar a estrela clicando [aqui](${link})`)
+                    .setColor(this.client.colors.default)
+                    .addField(`${this.client.emotes.sharo_excited} Surguiu uma estrela cadente!`, `Uma estrela cadente acabou de aparecer! Vá e pegue ela rápido antes que o tempo esgote!`)
+                    .addField(`${this.client.emotes.sharo_dying} Pegue rápido`, `🌠 Você pode pegar a estrela clicando [aqui](${link})`)
 
                 channel.send(starEmbed)
                 return
             }
             if (emoji.includes(reaction.emoji.name)) {
                 let embed = new MessageEmbed()
-                .setColor(this.client.colors.default)
-                .addField(`${this.client.emotes.sharo_happy} Apareceu um presente`, `Parece que o papai noel deixou um presente cair no ${reaction.message.channel}, corra enquanto da tempo, pegue o presente e seja feliz ${this.client.emotes.sharo_hug_chino}`)
-                .addField(`${this.client.emotes.sharo_dying} Pegue rápido`, `🎁 Você pode pegar o presente clicando [aqui](${link})`)
+                    .setColor(this.client.colors.default)
+                    .addField(`${this.client.emotes.sharo_happy} Apareceu um presente`, `Parece que o papai noel deixou um presente cair no ${reaction.message.channel}, corra enquanto da tempo, pegue o presente e seja feliz ${this.client.emotes.sharo_hug_chino}`)
+                    .addField(`${this.client.emotes.sharo_dying} Pegue rápido`, `🎁 Você pode pegar o presente clicando [aqui](${link})`)
 
                 channel.send(embed)
-                
+
                 return
             }
         }
