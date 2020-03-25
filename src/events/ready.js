ecb2bfc0c4e69ac5f50dc9cf529c719672ef18a5
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-03-24 21:59:50 -0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,17 +1,36 @@
+const { MessageEmbed } = require("discord.js")
 module.exports = class ReadyReceive {
     constructor(client) {
         this.client = client
     }
 
     run() {
-
-        console.log("Successfully connected")
-        this.client.user.setPresence({
-            activity: {
-                name: "Rabbit House Coffee ☕🐰",
+    	const embed = new MessageEmbed ()
+	embed.setColor(this.client.colors.default)
+	embed.setDescription("Sabia que outros servidores estão participando e apoiando o evento da Loritta? Não? Agora você está sabendo, logo abaixo, você encontrará os outros servidores participantes.")
+	embed.addField("Outros servidores", "[SweetLand](https://discord.gg/f6zz27a)")
+	setInterval(() => {
+            this.client.channels.get("659196915107495936").send(embed)
+        }, 1800000)
+	let stats =  [
+            {
+	        name: "Rabbit House Coffee ☕🐰",
                 type: "STREAMING",
-                url: "https://www.twitch.tv/danielagc"
-            }
-        })
+                url: "https://www.twitch.tv/danielagc" 
+	    },
+	    {
+		name: "🎉 Happy birthday Loritta",
+		type: "PLAYING"
+	    }
+
+	]
+        console.log("Successfully connected")
+        setInterval(() => {
+            this.client.user.setPresence({
+                activity: {
+                    stats[Math.floor(Math.random() * stats.length)]
+                }
+            })
+        }, 15000)
     }
 }
