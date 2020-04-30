3cf16667980c99b01837da0a883c7ef832a84d4c
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-04-29 23:04:23 -0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -9,20 +9,15 @@ module.exports = class ReadyReceive {
 	embed.setColor(this.client.colors.default)
 	embed.setDescription("Sabia que outros servidores estão participando e apoiando o evento da Loritta? Não? Agora você está sabendo, logo abaixo, você encontrará os outros servidores participantes.")
 	embed.addField("Outros servidores", "[SweetLand](https://discord.gg/7Gzy5Dm)")
-	setInterval(() => {
+	/* setInterval(() => {
             this.client.channels.cache.get("659196915107495936").send(embed)
-        }, 1800000)
+        }, 1800000) */
 	let stats = [
             {
 	        name: "Rabbit House Coffee ☕🐰",
                 type: "STREAMING",
                 url: "https://www.twitch.tv/danielagc" 
-	    },
-	    {
-		name: "🎉 Happy birthday Loritta",
-		type: "PLAYING"
 	    }
-
 	]
         console.log("Successfully connected")
         setInterval(() => {
